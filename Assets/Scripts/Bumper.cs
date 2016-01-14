96a9915d20687b1ea0a2c3ffcabffefd592cb549
Aidan Nabass <ai.nabass@gmail.com>
2016-01-13 18:18:28 -0800

    Initial Bumper Script

---
+++ Assets/Scripts/Bumper.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bumper : MonoBehaviour
+{
+    [Range (0,5)]
+    public int bumperForce;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        foreach (ContactPoint contact in collision.contacts)
+        {
+            contact.otherCollider.GetComponent<Rigidbody>().AddForce(-1 * contact.normal * bumperForce, ForceMode.Impulse);
+        }
+    }
+}
