fc41534a039ffb4cde14b382c2d710c1ecaa8b3a
Aidan Nabass <ai.nabass@gmail.com>
2016-01-14 14:53:08 -0800

    Implement Target Bumpers

--- Assets/Scripts/Bumper.cs
+++ Assets/Scripts/Bumper.cs
@@ -1,16 +1,41 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class Bumper : MonoBehaviour
 {
-    [Range (0,5)]
-    public int bumperForce;
+    public delegate void DOnHit(Bumper bumper);
 
-    void OnCollisionEnter(Collision collision)
+    public enum Styles
     {
-        foreach (ContactPoint contact in collision.contacts)
+        Default = 0,
+        Target = 1
+    }
+
+    [Range(0, 3)] public float BumperForce;
+
+    public Styles BumperStyles;
+
+    public DOnHit OnHit;
+
+    public bool IsObjectEnabled
+    {
+        get { return gameObject.activeSelf; }
+        set { gameObject.SetActive(value); }
+    }
+
+    private void Start()
+    {
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        collision.rigidbody.AddForce(-1*collision.contacts[0].normal*BumperForce, ForceMode.Impulse);
+        if (BumperStyles == Styles.Target)
         {
-            contact.otherCollider.GetComponent<Rigidbody>().AddForce(-1 * contact.normal * bumperForce, ForceMode.Impulse);
+            IsObjectEnabled = false;
+            if (OnHit != null)
+            {
+                OnHit(this);
+            }
         }
     }
 }
---
+++ Assets/Scripts/TPanelGroup.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TPanelGroup : MonoBehaviour
+{
+
+    private Bumper[] m_children = null;
+
+    private int ActiveChildren
+    {
+        get
+        {
+            int n = 0;
+            foreach (Bumper child in m_children)
+            {
+                if (child.IsObjectEnabled)
+                {
+                    ++n;
+                }
+            }
+            return n;
+        }
+    }
+
+	// Use this for initialization
+	void Start ()
+    {
+        m_children = GetComponentsInChildren<Bumper>();
+        Debug.Log("Current number of children: " + ActiveChildren);
+
+
+	    foreach (Bumper bumper in m_children)
+	    {
+            bumper.OnHit += OnHit;
+        }
+	}
+
+    private void OnHit(Bumper bumper)
+    {
+        DeactivateChild(bumper);
+        
+        
+    }
+
+    // Update is called once per frame
+	void Update () {
+	    if (ActiveChildren == 0)
+	    {
+	        StartCoroutine(ActivateChildren());
+	    }
+	}
+
+    void DeactivateChild(Bumper bumper)
+    {
+        bumper.IsObjectEnabled = false;
+        Debug.Log("Child Deactivated. Current Children: " + ActiveChildren);
+        Debug.Log(m_children.Length);
+    }
+
+    IEnumerator ActivateChildren()
+    {
+        yield return new WaitForSeconds(0.5f);
+        foreach (Bumper bumper in m_children)
+        {
+            bumper.IsObjectEnabled = true;
+        }
+    }
+}
