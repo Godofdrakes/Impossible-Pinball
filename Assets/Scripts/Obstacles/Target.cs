485c7d84ad0e691c4ea4a8b667a7309d2d93cbbe
Aidan Nabass <ai.nabass@gmail.com>
2016-01-15 12:16:16 -0800

    File Organization for Obstacles

---
+++ Assets/Scripts/Obstacles/Target.cs
@@ -0,0 +1,34 @@
+﻿using Obstacles;
+using UnityEngine;
+
+//[RequireComponent(typeof(Light))]
+public class Target : MonoBehaviour, IObstacle
+{
+    public Bumper Obstacle;
+    public MeshRenderer Light;
+
+    public bool IsObjectEnabled  {
+        get {
+            return Obstacle.IsObjectEnabled;
+        }
+        set {
+            if (value == false)
+            {
+                Obstacle.IsObjectEnabled = false;
+                SetColor(Color.yellow);
+            }
+            else
+            {
+                Obstacle.IsObjectEnabled = true;
+                SetColor(Color.blue);
+            }
+
+        }
+    }
+
+    public event DOnHit OnHit;
+
+    public void SetColor( Color c ) {
+        Light.material.SetColor("_EmissionColor", c );
+    }
+}
