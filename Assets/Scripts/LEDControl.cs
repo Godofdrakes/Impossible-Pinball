9a1a4200904fbc4b52a950742b71c61fdcfccc12
Aidan Nabass <ai.nabass@gmail.com>
2016-01-15 09:55:04 -0800

    Initial for target lights

---
+++ Assets/Scripts/LEDControl.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LEDControl : MonoBehaviour
+{
+
+    private Renderer renderer;
+    private Material light;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    renderer = GetComponent<Renderer>();
+	    light = renderer.material;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	    if (Input.GetKeyDown(KeyCode.L))
+	    {
+            light.SetColor("_EmissionColor", Color.blue);
+	    }
+
+	}
+}
