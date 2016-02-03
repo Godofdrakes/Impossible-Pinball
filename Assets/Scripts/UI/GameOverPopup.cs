ca278ff121d7536fc80d58aad9c9d29a2ace4464
Urias Rooney <Urias.Rooney@gmail.com>
2016-02-03 13:39:54 -0800

    Added GameOver screen.

---
+++ Assets/Scripts/UI/GameOverPopup.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+namespace Assets.Scripts.UI {
+
+    public class GameOverPopup : MonoBehaviour {
+
+        public void Exit() { Application.Quit(); }
+
+        public void MainMenu() { SceneManager.LoadScene( "Menu" ); }
+
+        public void Restart() { SceneManager.LoadScene( SceneManager.GetActiveScene().name ); }
+
+        public static void ShowGameOver() {
+            GameOverPopup popup = FindObjectOfType<GameOverPopup>();
+            popup.GetComponent<Image>().enabled = true;
+            popup.transform.GetChild( 0 ).gameObject.SetActive( true );
+        }
+
+    }
+
+}
